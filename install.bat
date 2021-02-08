cee7a246d9fffce54b86a640a51ea44b8109962a
unknown <oissac22@hotmail.com>
2021-02-08 11:02:49 -0300

    rename datas

--- install.bat
+++ install.bat
@@ -3,11 +3,11 @@
 cls
 echo instalando...
 
-docker rm -f ativa-mais-db
-docker image rm ativa-mais-db:latest
+docker rm -f mysql-db-digital
+docker image rm mysql-db-digital:latest
 
-docker rm -f ativa-mais-db-api
-docker image rm ativa-mais-db-api:latest
+docker rm -f mysql-db-digital-api
+docker image rm mysql-db-digital-api:latest
 
 
 
@@ -15,8 +15,8 @@ docker network create --subnet=172.18.0.0/16 api-db-network
 
 
 
-docker build -t ativa-mais-db:latest . -f Dockerfile.db
-docker run --net api-db-network --ip 172.18.0.22 -p 10073:3306 --name ativa-mais-db -d ativa-mais-db:latest
+docker build -t mysql-db-digital:latest . -f Dockerfile.db
+docker run --net api-db-network --ip 172.18.0.22 -p 10073:3306 --name mysql-db-digital -d mysql-db-digital:latest
 
-docker build -t ativa-mais-db-api:latest . -f Dockerfile.api
-docker run --net api-db-network --ip 172.18.0.23 -p 10074:80 --name ativa-mais-db-api -d ativa-mais-db-api:latest
+docker build -t mysql-db-digital-api:latest . -f Dockerfile.api
+docker run --net api-db-network --ip 172.18.0.23 -p 10074:80 --name mysql-db-digital-api -d mysql-db-digital-api:latest
